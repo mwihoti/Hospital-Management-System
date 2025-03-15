2668edeb09ae3b783ce1ef29d5632195fab4bc32
mwihoti <danielmwihoti@gmail.com>
2025-03-15 09:43:22 +0300

    Add user authentication and registration endpoints Implement NextAuth with credentialsProvider for user authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {label: "Username", type: "text", placeholder: "jsmith"},
+                password: {label: "Password", type: "password"},
+            }, 
+            async authorize(credentials, req) {
+                const user = { id: 1, name: "J Smith", email: "jsmith@example.com"}
+
+                if (user) {
+                    return user
+                } else {
+                    // if  return null then an error will be displayed
+                    return null 
+                }
+            }
+
+        })
+    ]
+})
+
+export { handler as GET, handler as POST }
---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import connectToDatabase from "@/lib/mongodb"
+import User from "@/models/User";
+
+export async function POST(request: Request) {
+    try {
+        await connectToDatabase()
+
+        const body = await request.json()
+        const { name, email, password, role, ...additionalFields } = body
+
+
+        // Check if user already
+        const existingUser = await User.findOne({email})
+        if (existingUser) {
+            return NextResponse.json({ error: "User with this email already exists"}, {status: 400})
+        }
+
+        // Validate role
+        if (!["admin", "doctor", "patient"].includes(role)) {
+            return NextResponse.json({ error: "Invalid role. Role must be an admin, patient and doctor"}, {status: 400})
+        }
+        // Validate role-specific required fields
+        if (role === "doctor" && (!additionalFields.specializtion || !additionalFields.department)) {
+            return NextResponse.json({error: "Doctors must provide spacialization and department"}, {status: 400})
+        }
+        // Create new user
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role,
+            ...additionalFields,
+        })
+
+        // Remove password from response
+        const userResponse = user.toObject()
+        delete userResponse.password
+
+        return NextResponse.json({ message: "User registered successfully", user: userResponse}, {status: 201})
+    } catch (error: any) {
+        console.error("Registration error:", error)
+        return NextResponse.json({ error: error.message || "Failed to register user"}, {status: 500})
+    }
+}
